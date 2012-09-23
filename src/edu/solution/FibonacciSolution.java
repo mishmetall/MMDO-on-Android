c2f810c1b641ed84b1f8932306e87c5ddb6497a6
Mihail Kuznetsov <misha_kuznetsov@ukr.net>
2012-09-23 23:31:36 +0300

    Change Fibonacci alhorythm

--- src/edu/solution/FibonacciSolution.java
+++ src/edu/solution/FibonacciSolution.java
@@ -47,9 +47,11 @@ public class FibonacciSolution implements ISolution,IIteraionTable
 		iterTable.clear();
 		
     	int j = 1, m;
-
-    	while(!(getNumber(j + 1) < (1/error)*(rightInterval - leftInterval) 
-    			&& (1/error)*(rightInterval - leftInterval) <= getNumber(j + 2)))
+    	double a0 = leftInterval;
+    	double b0 = rightInterval;
+    	
+    	while(!(getNumber(j + 1) < (1.0/error)*(b0 - a0) 
+    			&& (1/error)*(b0 - a0) <= getNumber(j + 2)))
     	{
     		j++;
 			if(j + 2 >= getLength())
@@ -57,29 +59,43 @@ public class FibonacciSolution implements ISolution,IIteraionTable
     	}
     	m = j;
     	
-    	double x1 = leftInterval + ((double)getNumber(m - 2)/getNumber(m))*(rightInterval - leftInterval);
-    	double x2 = leftInterval + ((double)getNumber(m - 1)/getNumber(m))*(rightInterval - leftInterval);
+    	double x1 = a0 + ((double)getNumber(m)/getNumber(m+2))*(b0 - a0);
+    	double x2 = a0 + ((double)getNumber(m + 1)/getNumber(m+2))*(b0 - a0);
     	double y1 = f.substitute(x1);
     	double y2 = f.substitute(x2);
+    	if(y1 > y2)
+	    	{	
+	    		leftInterval = x1;
+	    	}
+    	else
+    		{
+    			rightInterval = x2;
+    		}
     	int k = 1;
     	do 
     	{
 	    	if(y1 > y2)
-	    	{
-	    		leftInterval = x1;
+	    	{	    		
 	    		x1 = x2;
-	    		x2 = rightInterval - (x1 - leftInterval);
 	    		y1 = y2;
+	    		x2 = leftInterval + (double)getNumber(m-k+1)/getNumber(m+2)*(b0 - a0);
 	    		y2 = f.substitute(x2);
 	    	}
 	    	else 
 	    	{
-	    		rightInterval = x2;
 	    		x2 = x1;
-	    		x1 = leftInterval + (rightInterval - x2);
 	    		y2 = y1;
+	    		x1 = leftInterval + (double)getNumber(m-k)/getNumber(m+2)*(b0 - a0);
 	    		y1 = f.substitute(x1);
 	    	}
+	    	if(y1 > y2)
+		    	{	
+		    		leftInterval = x1;
+		    	}
+	    	else
+	    		{
+	    			rightInterval = x2;
+	    		}
 	    	k++;
 	    	iterTable.add(x1);
     	}
